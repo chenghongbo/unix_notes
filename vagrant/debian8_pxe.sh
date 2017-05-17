ac92b5ee54581b996ac1a3053e8ee73d53dd96b5
Alan Cheng <alan_cheng@epam.com>
2017-05-17 16:24:45 +0800

    add provision script for PXE server with debian 8

---
+++ vagrant/debian8_pxe.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+tftp_config_file='/etc/default/tftpd-hpa'
+dhcpd_conf='/etc/dhcp/dhcpd.conf'
+
+## download
+wget -O /tmp/erpxe.tar.gz http://sourceforge.net/projects/erpxe/files/latest/download?source=files
+cd /
+tar zxf /tmp/erpxe.tar.gz
+
+apt-get install -y tftpd-hpa apache2 nfs-kernel-server samba isc-dhcp-server
+update-rc.d tftpd-hpa defaults
+update-rc.d apache2 defaults
+update-rc.d nfs-kernel-server defaults
+update-rc.d samba defaults
+restorecon -r /tftpboot
+
+tftp_config='TFTP_USERNAME="tftp"
+TFTP_DIRECTORY="/tftpboot/"
+TFTP_ADDRESS="0.0.0.0:69"
+TFTP_OPTIONS="-m /tftpboot/erpxe.remap -vvvvv -s"'
+echo "$tftp_config" > $tftp_config_file
+
+## apache2 configuration
+cp /tftpboot/bin/setup/erpxe-httpd.conf /etc/apache2/conf-available/
+
+## NFS config
+cat /tftpboot/bin/setup/erpxe-exports > /etc/exports
+
+## Samba config
+cat /tftpboot/bin/setup/erpxe-smb.conf > /etc/samba/smb.conf
+
+## security=share deprecated in new version of samba
+sed -i 's/security = share/security = user/' /etc/samba/smb.conf
+sed -i '/security =  user/i \map to guest = bad user\n' /etc/samba/smb.conf
+
+useradd --no-create-home -s /dev/null erpxe
+(echo mypass;echo mypass)|smbpasswd -s -a erpxe
+(echo mypass;echo mypass)|smbpasswd -s -a root
+
+## DHCPD config
+dhcpd_config='
+ddns-update-style none;
+option domain-name "my.internal";
+option domain-name-servers 8.8.8.8, 8.8.4.4;
+default-lease-time 600;
+max-lease-time 7200;
+log-facility local7;
+subnet 10.5.5.0 netmask 255.255.255.0 {
+  range 10.5.5.26 10.5.5.130;
+  option domain-name-servers 8.8.8.8;
+  option domain-name "my.internal";
+  option routers 10.5.5.1;
+  option broadcast-address 10.5.5.255;
+  default-lease-time 600;
+  max-lease-time 7200;
+  next-server 10.5.5.1;
+  filename "pxelinux.0"; 
+}
+'
+echo "$dhcpd_config" > $dhcpd_conf
+
+## when used with vagrant/virtualbox and network is set 
+## to public in Vagrantfile, vagrant will create 2nd NIC
+## and it should be refered to as eth1 in Debian 8
+interface_config='
+auto eth1
+iface eth1 inet static
+  address 10.5.5.1
+  netmask 255.255.255.0
+'
+echo "$interface_config" >> /etc/network/interfaces
+
+## enable services
+for i in samba tftpd-hpa apache2 isc-dhcp-server
+do
+	systemctl start $i
+	systemctl enable $i
+done
